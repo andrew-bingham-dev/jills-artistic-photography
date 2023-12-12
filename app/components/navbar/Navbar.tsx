80b5f2f646b3ffab36ed01a986de1ba0170ba64d
Andrew <andrew.bingham.dev@gmail.com>
2023-12-12 21:43:21 +0700

    Create basic Navbar component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Section from "../section/Section";
 
 export default function Navbar() {
 	return (
-		<nav className="bg-red-500 flex items-center">
-			<Image src="/svg/logo.svg" alt="Logo" width={136} height={37} />
-			<ul className="flex">
-				<li>Home</li>
-				<li>Portfolio</li>
-				<li>About Me</li>
-				<li>Blog</li>
-			</ul>
-		</nav>
+		<Section>
+			<nav className="bg-red-500 flex items-center justify-between">
+				<Image src="/svg/logo.svg" alt="Logo" width={136} height={37} />
+				<ul className="flex gap-8">
+					<li>Home</li>
+					<li>Portfolio</li>
+					<li>About Me</li>
+					<li>Blog</li>
+				</ul>
+			</nav>
+		</Section>
 	);
 }
