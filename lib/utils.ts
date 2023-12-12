50c0cd839d5891d3ef91063bf01205077ee740bb
Andrew <andrew.bingham.dev@gmail.com>
2023-12-13 01:51:14 +0700

    Add capitalize function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function capitalizeFirstLetter(string: string) {
+  return string
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ")
 }
