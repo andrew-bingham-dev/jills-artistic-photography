8ab698a7f71e8fbf50af6a5b1dbe2c0599db10c4
Andrew <andrew.bingham.dev@gmail.com>
2023-12-13 00:37:04 +0700

    Update Section to take a bg color

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Navbar() {
 	return (
-		<Section>
+		<Section bgColor="my-light-blue">
 			<nav className="bg-my-light-blue flex items-center justify-between py-6">
 				<Image src="/svg/logo.svg" alt="Logo" width={136} height={37} />
 				<ul className="flex gap-8">
@@ -13,7 +13,7 @@ export default function Navbar() {
 					<li>About Me</li>
 					<li>Blog</li>
 				</ul>
-				<Button className="px-4 py-2 bg-my-dark-blue text-my-light-beige rounded-2xl">
+				<Button className="btn bg-my-dark-blue text-my-light-beige">
 					Contact Me
 				</Button>
 			</nav>
--- app/components/section/Section.tsx
+++ app/components/section/Section.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 interface SectionWrapperProps {
 	children: React.ReactNode;
+	bgColor: string;
 }
 
-const Section: React.FC<SectionWrapperProps> = ({ children }) => {
+const Section: React.FC<SectionWrapperProps> = ({ children, bgColor }) => {
 	return (
-		<section className="w-full max-w-7xl mx-auto px-8">{children}</section>
+		<section className={`w-full bg-${bgColor}`}>
+			<div className="max-w-7xl mx-auto">{children}</div>
+		</section>
 	);
 };
 
