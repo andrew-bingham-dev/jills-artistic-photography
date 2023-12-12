84efbec9b2ef3e4a062e0338868e3de857ddf9a0
Andrew <andrew.bingham.dev@gmail.com>
2023-12-13 01:51:28 +0700

    Create NavLink component

---
+++ app/components/navbar/NavLink.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface NavLinkProps {
+	label: string;
+	path: string;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ label, path }) => {
+	const currentPath = usePathname();
+
+	return (
+		<Link
+			href={path}
+			className={`${
+				currentPath === path ? "font-semibold" : ""
+			} hover:bg-black/5 hover:text-my-light-beige py-8 px-8 transition-colors duration-500`}
+		>
+			{label}
+		</Link>
+	);
+};
+
+export default NavLink;
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,17 +1,28 @@
+"use client";
+
 import Image from "next/image";
 import Section from "../section/Section";
 import { Button } from "@/components/ui/button";
+import NavLink from "./NavLink";
 
 export default function Navbar() {
 	return (
 		<Section bgColor="my-light-blue">
-			<nav className="bg-my-light-blue flex items-center justify-between py-6">
+			<nav className="bg-transparent flex items-center justify-between py-6 sticky">
 				<Image src="/svg/logo.svg" alt="Logo" width={136} height={37} />
-				<ul className="flex gap-8">
-					<li>Home</li>
-					<li>Portfolio</li>
-					<li>About Me</li>
-					<li>Blog</li>
+				<ul className="flex">
+					<li>
+						<NavLink label="Home" path="/" />
+					</li>
+					<li>
+						<NavLink label="Portfolio" path="/portfolio" />
+					</li>
+					<li>
+						<NavLink label="About Me" path="/about" />
+					</li>
+					<li>
+						<NavLink label="Blog" path="/blog" />
+					</li>
 				</ul>
 				<Button className="btn bg-my-dark-blue text-my-light-beige hover:bg-my-light-beige hover:text-my-dark-blue">
 					Contact Me
