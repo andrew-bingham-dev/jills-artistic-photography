5bdbbd81d1019a150d2d8b32a62521fd593dab5b
Andrew <andrew.bingham.dev@gmail.com>
2023-12-12 22:21:18 +0700

    Add button to Navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Section from "../section/Section";
+import { Button } from "@/components/ui/button";
 
 export default function Navbar() {
 	return (
 		<Section>
-			<nav className="bg-red-500 flex items-center justify-between">
+			<nav className="bg-my-light-blue flex items-center justify-between py-6">
 				<Image src="/svg/logo.svg" alt="Logo" width={136} height={37} />
 				<ul className="flex gap-8">
 					<li>Home</li>
@@ -12,6 +13,9 @@ export default function Navbar() {
 					<li>About Me</li>
 					<li>Blog</li>
 				</ul>
+				<Button className="px-4 py-2 bg-my-dark-blue text-my-light-beige rounded-2xl">
+					Contact Me
+				</Button>
 			</nav>
 		</Section>
 	);
