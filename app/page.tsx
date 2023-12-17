58d11fcc42577b323b2f711057ea5e7b030c299a
Andrew <andrew.bingham.dev@gmail.com>
2023-12-17 20:09:36 +0700

    Update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import CtaSection from "./components/cta-section/CtaSection";
+import FaqSection from "./components/faq-section/FaqSection";
+import Footer from "./components/footer-section/Footer";
 import HeroSection from "./components/hero-section/HeroSection";
 import Navbar from "./components/navbar/Navbar";
 import ServicesSection from "./components/services-section/ServicesSection";
+import TestimonialSection from "./components/testimonial-section/TestimonialSection";
 
 export default function Home() {
 	return (
@@ -8,6 +12,10 @@ export default function Home() {
 			<Navbar />
 			<HeroSection />
 			<ServicesSection />
+			<TestimonialSection />
+			<FaqSection />
+			<CtaSection />
+			<Footer />
 		</main>
 	);
 }
