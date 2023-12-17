79352fdbc18f4b156e88875c9b259c87622b040c
Andrew <andrew.bingham.dev@gmail.com>
2023-12-17 19:58:50 +0700

    Create Spacer

---
+++ app/components/Spacer.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+interface SpacerProps {
+	direction?: "horizontal" | "vertical";
+	size?: "small" | "medium" | "large" | "x-large";
+}
+
+const Spacer: React.FC<SpacerProps> = ({
+	direction = "vertical",
+	size = "medium",
+}) => {
+	const horizontalPadding = () => {
+		switch (size) {
+			case "small":
+				return "px-2";
+			case "medium":
+				return "px-4";
+			case "large":
+				return "px-8";
+			case "x-large":
+				return "px-16";
+			default:
+				return "";
+		}
+	};
+
+	const verticalPadding = () => {
+		switch (size) {
+			case "small":
+				return "py-2";
+			case "medium":
+				return "py-4";
+			case "large":
+				return "py-8";
+			case "x-large":
+				return "py-16";
+			default:
+				return "";
+		}
+	};
+
+	const spacerClassName =
+		direction === "horizontal" ? horizontalPadding() : verticalPadding();
+
+	return <div className={spacerClassName} />;
+};
+
+export default Spacer;
