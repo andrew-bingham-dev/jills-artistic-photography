eb2049a8a6bcdd5cb158ea77fac28a0a56f44fc3
Andrew <andrew.bingham.dev@gmail.com>
2023-12-18 12:07:00 +0700

    Fix RadialGradient

---
+++ app/components/radial-gradient/RadialGradient.tsx
@@ -0,0 +1,34 @@
+const RadialGradient: React.FC = () => {
+	return (
+		<div className={`absolute z-decor w-auto h-auto`}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="1665"
+				height="1665"
+				viewBox="-832.5 -832.5 1665 1665"
+				fill="none"
+			>
+				<path
+					d="M986 832.5C986 1292.28 613.277 1665 153.5 1665C-306.277 1665 -679 1292.28 -679 832.5C-679 372.723 -306.277 0 153.5 0C613.277 0 986 372.723 986 832.5Z"
+					fill="url(#paint0_radial_5842_5222)"
+					fill-opacity="0.4"
+				/>
+				<defs>
+					<radialGradient
+						id="customRadialGradient"
+						cx="0"
+						cy="0"
+						r="832.5"
+						gradientUnits="userSpaceOnUse"
+					>
+						<stop stop-color="#D9D9D9" />
+						<stop offset="1" stop-color="#D9D9D9" stop-opacity="0" />
+					</radialGradient>
+				</defs>
+				<circle cx="0" cy="0" r="832.5" fill="url(#customRadialGradient)" />
+			</svg>
+		</div>
+	);
+};
+
+export default RadialGradient;
--- app/components/services-section/PhotoSpread.tsx
+++ app/components/services-section/PhotoSpread.tsx
@@ -1,21 +1,22 @@
 import Image from "next/image";
 
-import natureImage01 from "../../../public/jpg/nature/nature_01.jpg";
-import natureImage05 from "../../../public/jpg/nature/nature_05.jpg";
-import portraitImage02 from "../../../public/jpg/portrait/portrait_02.jpg";
-import portraitImage05 from "../../../public/jpg/portrait/portrait_05.jpg";
-import portraitImage09 from "../../../public/jpg/portrait/portrait_09.jpg";
-import weddingImage01 from "../../../public/jpg/wedding/wedding_01.jpg";
-import weddingImage02 from "../../../public/jpg/wedding/wedding_02.jpg";
+import RadialGradient from "../radial-gradient/RadialGradient";
 
 const PhotoSpread = () => {
 	return (
-		// TODO: set this sections aspect ration
+		// TODO: Fix the gradient circle so that it's centered on the image
 		<div className="relative aspect-[5/3.5]">
+			{/* gradient circle */}
+			<div
+				id="gradient"
+				className="absolute left-[30%] top-[40%] z-decor flex justify-center items-center w-auto h-auto opacity-50"
+			>
+				<RadialGradient />
+			</div>
 			{/* w:416px h:234px - w:29% */}
-			<div className="absolute w-[29%] top-[2%] left-[4%] -rotate-12 overflow-clip aspect-[3/2] shadow-md">
+			<div className="absolute w-[29%] top-[2%] left-[4%] -rotate-12 overflow-clip aspect-[3/2] shadow-md z-image">
 				<Image
-					src={portraitImage09}
+					src={"/jpg/portrait/portrait_01.jpg"}
 					alt="photo example 1"
 					objectFit="cover"
 					layout="fill"
@@ -24,9 +25,9 @@ const PhotoSpread = () => {
 			</div>
 
 			{/* w:416px h:415px - w:29% */}
-			<div className="absolute w-[29%] aspect-square left-[24%] top-[5%] -rotate-2  shadow-md">
+			<div className="absolute w-[29%] aspect-square left-[24%] top-[5%] -rotate-2 shadow-md z-image">
 				<Image
-					src={portraitImage05}
+					src={"/jpg/portrait/portrait_05.jpg"}
 					alt="photo example 2"
 					objectFit="cover"
 					layout="fill"
@@ -35,9 +36,9 @@ const PhotoSpread = () => {
 			</div>
 
 			{/* w:416px h:234px - w:29% */}
-			<div className="absolute w-[29%] aspect-[2/1] right-[6%] top-[9%] rotate-6  shadow-md">
+			<div className="absolute w-[29%] aspect-[2/1] right-[6%] top-[9%] rotate-6 shadow-md z-image">
 				<Image
-					src={portraitImage02}
+					src={"/jpg/portrait/portrait_02.jpg"}
 					alt="photo example 3"
 					objectFit="cover"
 					layout="fill"
@@ -46,9 +47,9 @@ const PhotoSpread = () => {
 			</div>
 
 			{/* w:416px h:416px - w:29% */}
-			<div className="absolute w-[29%] aspect-square left-[5%] top-[24%] rotate-12  shadow-md">
+			<div className="absolute w-[29%] aspect-square left-[5%] top-[24%] rotate-12 shadow-md z-image">
 				<Image
-					src={natureImage01}
+					src={"/jpg/nature/nature_01.jpg"}
 					alt="photo example 4"
 					objectFit="cover"
 					layout="fill"
@@ -57,9 +58,9 @@ const PhotoSpread = () => {
 			</div>
 
 			{/* w:416px h:416px - w:29% */}
-			<div className="absolute w-[29%] aspect-square left-[50%] top-[23%] rotate-[25deg]  shadow-md">
+			<div className="absolute w-[29%] aspect-square left-[43%] top-[23%] rotate-[25deg] shadow-md z-image">
 				<Image
-					src={natureImage05}
+					src={"/jpg/nature/nature_05.jpg"}
 					alt="photo example 5"
 					objectFit="cover"
 					layout="fill"
@@ -68,9 +69,9 @@ const PhotoSpread = () => {
 			</div>
 
 			{/* w:515px h:515px - w40% */}
-			<div className="absolute w-[35%] aspect-square left-[15%] top-[42%] -rotate-[6deg]  shadow-md">
+			<div className="absolute w-[35%] aspect-square left-[15%] top-[42%] -rotate-[6deg] shadow-md z-image">
 				<Image
-					src={weddingImage02}
+					src={"/jpg/wedding/wedding_02.jpg"}
 					alt="photo example 6"
 					objectFit="cover"
 					layout="fill"
@@ -79,9 +80,9 @@ const PhotoSpread = () => {
 			</div>
 
 			{/* w:712px h:712px - w:29% */}
-			<div className="absolute w-[49%] aspect-square left-[48%] top-[45%] rotate-[9deg]  shadow-md">
+			<div className="absolute w-[49%] aspect-square left-[50%] top-[45%] rotate-[9deg] shadow-md z-image">
 				<Image
-					src={weddingImage01}
+					src={"/jpg/wedding/wedding_01.jpg"}
 					alt="photo example 7"
 					objectFit="cover"
 					layout="fill"
