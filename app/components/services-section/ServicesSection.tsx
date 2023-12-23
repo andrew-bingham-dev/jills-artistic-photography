e9da7842efe9f52745d7c7b1cc277e31cd2a2acb
Andrew <andrew.bingham.dev@gmail.com>
2023-12-23 14:18:17 +0700

    Refactor ServicesSection component layout

--- app/components/services-section/ServicesSection.tsx
+++ app/components/services-section/ServicesSection.tsx
@@ -7,16 +7,17 @@ import PhotoSpread from './PhotoSpread'
 const ServicesSection = () => {
    return (
       <Section name="ServicesSection" bgColor="my-medium-blue">
-         <div className="1450:px-0 px-[4vw] py-[8vw]">
-            <BrowHeadingParagraph
-               color="my-light-beige"
-               brow="Capture"
-               heading="Preserve Your Precious Moments"
-               paragraph="Professional photography services for weddings, portraits, and more."
-            />
-            <Spacer size="large" />
-            <ServicesCardContainer />
-
+         <div className="pt-[8vw]">
+            <div className="px-[4vw] 1450:px-0">
+               <BrowHeadingParagraph
+                  color="my-light-beige"
+                  brow="Capture"
+                  heading="Preserve Your Precious Moments"
+                  paragraph="Professional photography services for weddings, portraits, and more."
+               />
+               <Spacer size="large" />
+               <ServicesCardContainer />
+            </div>
             <PhotoSpread />
          </div>
       </Section>
