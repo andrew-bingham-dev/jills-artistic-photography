ab616af6c1802875855e1861f0a0f32ef5320025
Andrew <andrew.bingham.dev@gmail.com>
2023-12-23 15:19:07 +0700

    Refactor CtaSection component structure and styling

--- app/components/cta-section/CtaSection.tsx
+++ app/components/cta-section/CtaSection.tsx
@@ -1,26 +1,24 @@
-import Spacer from "../Spacer";
-import Section from "../section/Section";
-import CtaButton from "../cta-button/CtaButton";
+import Spacer from '../Spacer'
+import Section from '../section/Section'
+import CtaButton from '../cta-button/CtaButton'
 
 const CtaSection: React.FC = () => {
-  return (
-    <Section name="CtaSection" bgColor="my-medium-blue">
-      <Spacer size="x-large" />
-      <div className="grid grid-cols-2 grid-rows-1 text-my-light-beige">
-        <h2>Capture Your Special Moments</h2>
-        <div className="flex flex-col gap-y-8">
-          <p>
-            I&apos;m Jill Thompson, your go-to freelance photographer for
-            capturing life&apos;s most beautiful moments. From weddings and
-            portraits to nature scenes, I&apos;m committed to creating images
-            you&apos;ll treasure for a lifetime.
-          </p>
-          <CtaButton />
-        </div>
-      </div>
-      <Spacer size="x-large" />
-    </Section>
-  );
-};
+   return (
+      <Section name="CtaSection" bgColor="my-medium-blue">
+         <div className="gap-y-40x6x80 grid auto-rows-auto px-[4vw] py-[8vw] text-my-light-beige 800:grid-cols-2 800:grid-rows-1 1450:px-0">
+            <h2 className="text-h2">Capture Your Special Moments</h2>
+            <div className="gap-16x4x24 flex flex-col">
+               <p className="text-para">
+                  I&apos;m Jill Thompson, your go-to freelance photographer for
+                  capturing life&apos;s most beautiful moments. From weddings
+                  and portraits to nature scenes, I&apos;m committed to creating
+                  images you&apos;ll treasure for a lifetime.
+               </p>
+               <CtaButton />
+            </div>
+         </div>
+      </Section>
+   )
+}
 
-export default CtaSection;
+export default CtaSection
