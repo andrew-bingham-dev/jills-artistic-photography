636f68ab6725fe0651759eb7d50d03bbea383559
Andrew <andrew.bingham.dev@gmail.com>
2023-12-23 14:18:38 +0700

    Refactor Testimonial component

--- app/components/testimonial-section/Testimonial.tsx
+++ app/components/testimonial-section/Testimonial.tsx
@@ -1,29 +1,29 @@
-import starSvg from "../../../public/svg/Star.svg";
-import Image, { StaticImageData } from "next/image";
+import starSvg from '../../../public/svg/Star.svg'
+import Image, { StaticImageData } from 'next/image'
 
 interface Testimonial {
-	quote: string;
-	avatar: StaticImageData;
-	name: string;
+   quote: string
+   avatar: StaticImageData
+   name: string
 }
 
 const Testimonial: React.FC<Testimonial> = ({ quote, avatar, name }) => {
-	return (
-		<div className="flex flex-col gap-8 text-my-light-beige">
-			<div className="flex flex-row gap-1">
-				<Image src={starSvg} alt="Star" />
-				<Image src={starSvg} alt="Star" />
-				<Image src={starSvg} alt="Star" />
-				<Image src={starSvg} alt="Star" />
-				<Image src={starSvg} alt="Star" />
-			</div>
-			<p className="italic">&quot;{quote}&quot;</p>
-			<div className="flex items-center gap-4">
-				<Image src={avatar} alt="Avatar" />
-				<p className="font-semibold">{name}</p>
-			</div>
-		</div>
-	);
-};
+   return (
+      <div className="gap-16x4x24 flex flex-col text-my-light-beige">
+         <div className="flex flex-row gap-1">
+            <Image src={starSvg} alt="Star" />
+            <Image src={starSvg} alt="Star" />
+            <Image src={starSvg} alt="Star" />
+            <Image src={starSvg} alt="Star" />
+            <Image src={starSvg} alt="Star" />
+         </div>
+         <p className="text-para italic">&quot;{quote}&quot;</p>
+         <div className="flex items-center gap-4">
+            <Image src={avatar} alt="Avatar" />
+            <p className="font-semibold">{name}</p>
+         </div>
+      </div>
+   )
+}
 
-export default Testimonial;
+export default Testimonial
