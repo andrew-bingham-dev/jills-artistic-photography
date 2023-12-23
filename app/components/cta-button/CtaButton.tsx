8e6902b2482429a6fc034cd9a3ba190994b031a2
Andrew <andrew.bingham.dev@gmail.com>
2023-12-23 14:17:00 +0700

    Refactor CtaButton text scaling

--- app/components/cta-button/CtaButton.tsx
+++ app/components/cta-button/CtaButton.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 const CtaButton = () => {
-	return (
-		<Button className="btn bg-my-medium-orange max-w-fit text-my-dark-blue hover:bg-my-dark-blue hover:text-my-medium-orange shadow-xl">
-			Contact Me
-		</Button>
-	);
-};
+   return (
+      <Button className="btn text-button max-w-fit bg-my-medium-orange text-my-dark-blue shadow-xl hover:bg-my-dark-blue hover:text-my-medium-orange">
+         Contact Me
+      </Button>
+   )
+}
 
-export default CtaButton;
+export default CtaButton
