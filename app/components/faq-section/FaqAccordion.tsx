0eebe063a22e5f9a99c410942e65af9bc6a25ab9
Andrew <andrew.bingham.dev@gmail.com>
2023-12-23 15:19:24 +0700

    Refactor FaqAccordion and FaqSection components

--- app/components/faq-section/FaqAccordion.tsx
+++ app/components/faq-section/FaqAccordion.tsx
@@ -1,77 +1,77 @@
 import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
-} from "@/components/ui/accordion";
+   Accordion,
+   AccordionContent,
+   AccordionItem,
+   AccordionTrigger,
+} from '@/components/ui/accordion'
 
 const FaqAccordion = () => {
-	return (
-		<Accordion type="single" collapsible>
-			<AccordionItem value="item 1" className="border-my-dark-blue">
-				<AccordionTrigger className="font-semibold">
-					How do I book a session?
-				</AccordionTrigger>
-				<AccordionContent className="text-lg">
-					Booking a session with me is easy! Just head over to the
-					&apos;Contact&apos; page, fill out your details, and let me know
-					what type of photography session you&apos;re interested in.
-					I&apos;ll get back to you within 24 hours to discuss the details
-					and set a date!
-				</AccordionContent>
-			</AccordionItem>
+   return (
+      <Accordion type="single" collapsible>
+         <AccordionItem value="item 1" className="border-my-dark-blue">
+            <AccordionTrigger className="text-para text-left font-semibold">
+               How do I book a session?
+            </AccordionTrigger>
+            <AccordionContent className="text-para">
+               Booking a session with me is easy! Just head over to the
+               &apos;Contact&apos; page, fill out your details, and let me know
+               what type of photography session you&apos;re interested in.
+               I&apos;ll get back to you within 24 hours to discuss the details
+               and set a date!
+            </AccordionContent>
+         </AccordionItem>
 
-			<AccordionItem value="item 2" className="border-my-dark-blue">
-				<AccordionTrigger className="font-semibold">
-					What are your rates?
-				</AccordionTrigger>
-				<AccordionContent className="text-lg">
-					My rates vary depending on the type and length of the photography
-					session. For a detailed price list, please send me a message via
-					the &apos;Contact&apos; page. I offer packages tailored to
-					various needs and budgets, ensuring you get the best value for
-					stunning, high-quality photos.
-				</AccordionContent>
-			</AccordionItem>
+         <AccordionItem value="item 2" className="border-my-dark-blue">
+            <AccordionTrigger className="text-para text-left font-semibold">
+               What are your rates?
+            </AccordionTrigger>
+            <AccordionContent className="text-para">
+               My rates vary depending on the type and length of the photography
+               session. For a detailed price list, please send me a message via
+               the &apos;Contact&apos; page. I offer packages tailored to
+               various needs and budgets, ensuring you get the best value for
+               stunning, high-quality photos.
+            </AccordionContent>
+         </AccordionItem>
 
-			<AccordionItem value="item 3" className="border-my-dark-blue">
-				<AccordionTrigger className="font-semibold">
-					Do you travel for weddings?
-				</AccordionTrigger>
-				<AccordionContent className="text-lg">
-					Absolutely! I love capturing love stories in new and exciting
-					locations. Travel fees may apply for venues outside a certain
-					radius from my base location. For more specific information about
-					travel rates, please contact me directly.
-				</AccordionContent>
-			</AccordionItem>
+         <AccordionItem value="item 3" className="border-my-dark-blue">
+            <AccordionTrigger className="text-para text-left font-semibold">
+               Do you travel for weddings?
+            </AccordionTrigger>
+            <AccordionContent className="text-para">
+               Absolutely! I love capturing love stories in new and exciting
+               locations. Travel fees may apply for venues outside a certain
+               radius from my base location. For more specific information about
+               travel rates, please contact me directly.
+            </AccordionContent>
+         </AccordionItem>
 
-			<AccordionItem value="item 4" className="border-my-dark-blue">
-				<AccordionTrigger className="font-semibold">
-					How long until I receive my photos?
-				</AccordionTrigger>
-				<AccordionContent className="text-lg">
-					I understand how eager you must be to see your photos! My usual
-					turnaround time for delivering the final edited images is about
-					4-6 weeks after your event. I take great care in editing each
-					photo to perfection, ensuring you receive the best quality.
-				</AccordionContent>
-			</AccordionItem>
+         <AccordionItem value="item 4" className="border-my-dark-blue">
+            <AccordionTrigger className="text-para text-left font-semibold">
+               How long until I receive my photos?
+            </AccordionTrigger>
+            <AccordionContent className="text-para">
+               I understand how eager you must be to see your photos! My usual
+               turnaround time for delivering the final edited images is about
+               4-6 weeks after your event. I take great care in editing each
+               photo to perfection, ensuring you receive the best quality.
+            </AccordionContent>
+         </AccordionItem>
 
-			<AccordionItem value="item 5" className="border-my-dark-blue">
-				<AccordionTrigger className="font-semibold">
-					Can I order prints?
-				</AccordionTrigger>
-				<AccordionContent className="text-lg">
-					Yes, you can! Along with your digital gallery, I offer a variety
-					of high-quality print options. From traditional photo prints to
-					canvas and framed art, you can choose the perfect way to display
-					your memories. Information on print options and pricing will be
-					available with your final gallery.
-				</AccordionContent>
-			</AccordionItem>
-		</Accordion>
-	);
-};
+         <AccordionItem value="item 5" className="border-my-dark-blue">
+            <AccordionTrigger className="text-para text-left font-semibold">
+               Can I order prints?
+            </AccordionTrigger>
+            <AccordionContent className="text-para">
+               Yes, you can! Along with your digital gallery, I offer a variety
+               of high-quality print options. From traditional photo prints to
+               canvas and framed art, you can choose the perfect way to display
+               your memories. Information on print options and pricing will be
+               available with your final gallery.
+            </AccordionContent>
+         </AccordionItem>
+      </Accordion>
+   )
+}
 
-export default FaqAccordion;
+export default FaqAccordion
--- app/components/faq-section/FaqSection.tsx
+++ app/components/faq-section/FaqSection.tsx
@@ -1,29 +1,27 @@
-import Spacer from "../Spacer";
-import BrowHeadingParagraph from "../brow-heading-paragraph/BrowHeadingParagraph";
-import Section from "../section/Section";
-import FaqAccordion from "./FaqAccordion";
+import Spacer from '../Spacer'
+import BrowHeadingParagraph from '../brow-heading-paragraph/BrowHeadingParagraph'
+import Section from '../section/Section'
+import FaqAccordion from './FaqAccordion'
 
 const FaqSection: React.FC = () => {
-	return (
-		<Section name="FAQSection" bgColor="my-light-blue">
-			<Spacer size="x-large" />
-			<BrowHeadingParagraph
-				color="my-dark-blue"
-				heading="FAQs"
-				paragraph="Find answers to common questions about my services and process."
-			/>
-			<Spacer size="large" />
-			<FaqAccordion />
-			<Spacer size="large" />
-			<BrowHeadingParagraph
-				headingType="h4"
-				color="my-dark-blue"
-				heading="Still Have Questions?"
-				paragraph="Contact me directly to discuss your photography needs."
-			/>
-			<Spacer size="x-large" />
-		</Section>
-	);
-};
+   return (
+      <Section name="FAQSection" bgColor="my-light-blue">
+         <div className="gap-y-40x6x80 flex flex-col px-[4vw] py-[8vw] 1450:px-0">
+            <BrowHeadingParagraph
+               color="my-dark-blue"
+               heading="FAQs"
+               paragraph="Find answers to common questions about my services and process."
+            />
+            <FaqAccordion />
+            <BrowHeadingParagraph
+               headingType="h4"
+               color="my-dark-blue"
+               heading="Still Have Questions?"
+               paragraph="Contact me directly to discuss your photography needs."
+            />
+         </div>
+      </Section>
+   )
+}
 
-export default FaqSection;
+export default FaqSection
