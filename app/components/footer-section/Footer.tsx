88f895564f992d85e92e24a17a03eeb7d8a23399
Andrew <andrew.bingham.dev@gmail.com>
2023-12-23 15:45:34 +0700

    Refactor footer section component

--- app/components/footer-section/Footer.tsx
+++ app/components/footer-section/Footer.tsx
@@ -1,35 +1,39 @@
-import Spacer from "../Spacer";
-import Section from "../section/Section";
-import Logo from "../logo/Logo";
-import SocialIcons from "../social-icons/SocialIcons";
+import Spacer from '../Spacer'
+import Section from '../section/Section'
+import Logo from '../logo/Logo'
+import SocialIcons from '../social-icons/SocialIcons'
 
 const Footer: React.FC = () => {
-	return (
-		<Section name="Footer" bgColor="my-dark-blue">
-			<Spacer size="x-large" />
-			<div className="flex justify-between text-my-light-beige">
-				<Logo type="light" />
-				<ul className="flex gap-8 ">
-					<li>Home</li>
-					<li>Portfolio</li>
-					<li>About Me</li>
-					<li>Blog</li>
-				</ul>
-				<SocialIcons />
-			</div>
-			<Spacer size="large" />
-			<hr />
-			<Spacer size="large" />
-			<div className="text-my-light-beige flex justify-between">
-				<a href="#">
-					© 2023 Jill&apos;s Artistic Photography. All Rights Reserved.
-				</a>
-				<a href="#">Website by Koder</a>
-			</div>
+   return (
+      <Section name="Footer" bgColor="my-dark-blue">
+         <div className="gap-y-40x6x80 flex flex-col px-[4vw] py-[8vw] 1450:px-0">
+            <div className="flex flex-col items-center gap-[8vw] text-center text-my-light-beige 900:flex-row 900:justify-between">
+               <div className="translate-x-[50.015px] 900:translate-x-0">
+                  <Logo type="light" />
+               </div>
+               <ul className="flex flex-col gap-[4vw] 900:flex-row">
+                  <li>Home</li>
+                  <li>Portfolio</li>
+                  <li>About Me</li>
+                  <li>Blog</li>
+               </ul>
+               <SocialIcons />
+            </div>
+            <hr />
+            <div className="flex flex-col justify-between gap-[4vw] text-center text-my-light-beige 900:flex-row">
+               <a href="#">
+                  <p className="text-para">
+                     © 2023 Jill&apos;s Artistic Photography. All Rights
+                     Reserved.
+                  </p>
+               </a>
+               <a href="#">
+                  <p className="text-para">Website by Koder</p>
+               </a>
+            </div>
+         </div>
+      </Section>
+   )
+}
 
-			<Spacer size="x-large" />
-		</Section>
-	);
-};
-
-export default Footer;
+export default Footer
