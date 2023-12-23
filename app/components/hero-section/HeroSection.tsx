be48fed427f42f0640769571be45f7f5e0675cb1
Andrew <andrew.bingham.dev@gmail.com>
2023-12-23 12:17:32 +0700

    Refactor HeroSection component styles

--- app/components/hero-section/HeroSection.tsx
+++ app/components/hero-section/HeroSection.tsx
@@ -17,13 +17,13 @@ const HeroSection = () => {
          </div>
 
          {/* Hero text and image */}
-         <div className="600:grid-cols-2 800:min-h-[450px] 1000:min-h-[500px] grid auto-rows-auto bg-my-light-blue">
+         <div className="grid auto-rows-auto bg-my-light-blue 600:grid-cols-2 800:min-h-[450px] 1000:min-h-[500px]">
             {/* Hero text */}
-            <div className="600:order-1 600:pl-[4vw] 600:pr-0 1450:pl-0 z-10 order-2 flex flex-col justify-center gap-6 px-[4vw] py-[8vw]">
-               <h1 className="title-text-mark 900:text-[56px] 1450:text-[64px] text-[40px] leading-none text-my-dark-blue">
+            <div className="z-10 order-2 flex flex-col justify-center gap-6 px-[4vw] py-[8vw] 600:order-1 600:pl-[4vw] 600:pr-0 1450:pl-0">
+               <h1 className="title-text-mark text-h1 leading-none text-my-dark-blue">
                   Capturing Moments That Last a Lifetime
                </h1>
-               <p className="1450:text-[32px] text-[18px] font-light leading-tight  text-my-dark-blue">
+               <p className="text-h1-para font-light leading-tight text-my-dark-blue">
                   Welcome to Jill&apos;s Artistic Photography Where Your
                   Treasured Moments Become Timeless Masterpieces You&apos;ll
                   Cherish Forever.
@@ -32,8 +32,8 @@ const HeroSection = () => {
             </div>
 
             {/* Hero image */}
-            <div className="600:order-2 relative z-[2] order-1">
-               <div className="600:absolute 600:-top-[4vw] 600:aspect-square 1000:max-h-[700px] 1000:max-w-[700px] 600:h-[70vw] 600:min-h-[520px] 600:w-[70vw] 600:min-w-[520px] 600:rounded-full 1450:max-h-[850px] 1450:max-w-[850px] h-[60vw] w-full overflow-clip">
+            <div className="relative z-[2] order-1 600:order-2">
+               <div className="h-[60vw] min-h-[250px] w-full overflow-clip 600:absolute 600:-top-[4vw] 600:aspect-square 600:h-[70vw] 600:min-h-[520px] 600:w-[70vw] 600:min-w-[520px] 600:rounded-full 1000:max-h-[700px] 1000:max-w-[700px] 1450:max-h-[850px] 1450:max-w-[850px]">
                   <Image
                      src={'/jpg/profile/profile_01.jpg'}
                      alt="Hero Image"
