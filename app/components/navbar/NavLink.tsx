40b3467a9c978e07f625e03443bdc5c8587a59b1
Andrew <andrew.bingham.dev@gmail.com>
2023-12-23 12:18:34 +0700

    Refactor navbar component

--- app/components/navbar/NavLink.tsx
+++ app/components/navbar/NavLink.tsx
@@ -1,24 +1,24 @@
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 interface NavLinkProps {
-	label: string;
-	path: string;
+   label: string
+   path: string
 }
 
 const NavLink: React.FC<NavLinkProps> = ({ label, path }) => {
-	const currentPath = usePathname();
+   const currentPath = usePathname()
 
-	return (
-		<Link
-			href={path}
-			className={`${
-				currentPath === path ? "font-semibold" : ""
-			} hover:bg-black/5 py-8 md:px-4 lg:px-8 transition-colors duration-500`}
-		>
-			{label}
-		</Link>
-	);
-};
+   return (
+      <Link
+         href={path}
+         className={`${
+            currentPath === path ? 'font-semibold' : ''
+         } 1000:px-8 px-4 py-8 transition-colors duration-500 hover:bg-black/5`}
+      >
+         {label}
+      </Link>
+   )
+}
 
-export default NavLink;
+export default NavLink
