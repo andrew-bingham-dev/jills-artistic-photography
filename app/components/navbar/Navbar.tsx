782386273780ab43bb1c34e4bb934b3d4e52f96e
Andrew <andrew.bingham.dev@gmail.com>
2023-12-24 23:00:15 +0700

    Add framer-motion animations to Navbar component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,11 @@
 'use client'
 
+import {
+   motion,
+   useMotionValue,
+   useMotionValueEvent,
+   useScroll,
+} from 'framer-motion'
 import { useState } from 'react'
 import NavLink from './NavLink'
 import { Button } from '@/components/ui/button'
@@ -10,20 +16,48 @@ import '../../../styles/burger.css'
 
 export default function Navbar() {
    const [isMenuOpen, setIsMenuOpen] = useState(false)
+   const [isNavVisible, setIsNavVisible] = useState(true)
+   const { scrollY } = useScroll()
+
+   useMotionValueEvent(scrollY, 'change', (latest) => {
+      if (latest < scrollY.getPrevious()) {
+         setIsNavVisible(true)
+      } else if (latest > scrollY.getPrevious()) {
+         setIsNavVisible(false)
+      }
+   })
 
    const toggleMenu = () => {
       setIsMenuOpen(!isMenuOpen)
    }
 
+   const navbar = { hidden: { y: -88 }, visible: { y: 0 } }
+
    return (
-      <div
+      <motion.div
+         layout
          id="NavbarSection"
-         className="sticky top-0 z-nav bg-my-light-blue px-[4vw] 1450:px-0"
+         className="fixed top-0 z-nav w-full bg-my-light-blue px-[4vw] 1450:px-0"
+         initial={{ y: -88 }}
+         animate={isNavVisible ? 'visible' : 'hidden'}
+         transition={{ duration: 0.3, ease: 'easeInOut' }}
+         variants={navbar}
       >
          {/* Desktop Navbar */}
          <nav className="mx-auto hidden max-w-7xl items-center justify-between bg-transparent py-6 700:flex">
-            <Logo type="dark" />
-            <ul className="flex ">
+            <motion.div
+               initial={{ x: -100, opacity: 0 }}
+               animate={{ x: 0, opacity: 1 }}
+               transition={{ type: 'spring', delay: 0.2 }}
+            >
+               <Logo type="dark" />
+            </motion.div>
+            <motion.ul
+               initial={{ y: -88, opacity: 0 }}
+               animate={{ y: 0, opacity: 1 }}
+               transition={{ type: 'spring', delay: 0.2 }}
+               className="flex "
+            >
                <li>
                   <NavLink label="Home" path="/" />
                </li>
@@ -36,24 +70,46 @@ export default function Navbar() {
                <li>
                   <NavLink label="Blog" path="/blog" />
                </li>
-            </ul>
-            <Button className="btn text-button bg-my-dark-blue text-my-light-beige shadow-xl hover:bg-my-light-beige hover:text-my-dark-blue">
-               Contact Me
-            </Button>
+            </motion.ul>
+            <motion.div
+               initial={{ x: 100, opacity: 0 }}
+               animate={{ x: 0, opacity: 1 }}
+               transition={{ type: 'spring', delay: 0.2 }}
+            >
+               <Button className="btn bg-my-dark-blue text-button text-my-light-beige shadow-xl hover:bg-my-light-beige hover:text-my-dark-blue">
+                  Contact Me
+               </Button>
+            </motion.div>
          </nav>
 
          {/* Mobile Navbar */}
          <nav className="mx-auto grid max-w-7xl grid-cols-3 items-center bg-transparent py-6 700:hidden">
-            <div className="justify-self-start">
+            <motion.div
+               initial={{ x: -100, opacity: 0 }}
+               animate={{ x: 0, opacity: 1 }}
+               transition={{ type: 'spring', delay: 0.2 }}
+               className="justify-self-start"
+            >
                <Logo type="dark" />
-            </div>
-            <Button className="btn justify-self-center bg-my-dark-blue text-my-light-beige shadow-xl hover:bg-my-light-beige hover:text-my-dark-blue">
-               Contact Me
-            </Button>
-            <div className="justify-self-end">
+            </motion.div>
+            <motion.div
+               initial={{ y: -100, opacity: 0 }}
+               animate={{ y: 0, opacity: 1 }}
+               transition={{ type: 'spring', delay: 0.2 }}
+            >
+               <Button className="btn justify-self-center bg-my-dark-blue text-my-light-beige shadow-xl hover:bg-my-light-beige hover:text-my-dark-blue">
+                  Contact Me
+               </Button>
+            </motion.div>
+            <motion.div
+               initial={{ x: 100, opacity: 0 }}
+               animate={{ x: 0, opacity: 1 }}
+               transition={{ type: 'spring', delay: 0.2 }}
+               className="justify-self-end"
+            >
                <Burger onClick={toggleMenu} isOpen={isMenuOpen} />
-            </div>
+            </motion.div>
          </nav>
-      </div>
+      </motion.div>
    )
 }
