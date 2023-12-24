b840ac28b2a6a492af406d24e34c93e5c1fe77dc
Andrew <andrew.bingham.dev@gmail.com>
2023-12-24 23:39:15 +0700

    Add motion effects to HeroSection component

--- app/components/hero-section/HeroSection.tsx
+++ app/components/hero-section/HeroSection.tsx
@@ -1,13 +1,16 @@
+'use client'
+
 import Section from '../section/Section'
 import Image from 'next/image'
 import CtaButton from '../cta-button/CtaButton'
+import { motion } from 'framer-motion'
 
 const HeroSection = () => {
    return (
       <Section
          name="HeroSection"
          bgColor="my-light-blue"
-         sectionClasses="overflow-y-clip"
+         sectionClasses="overflow-y-clip max-h-[800px]"
       >
          {/* Background circles */}
          <div>
@@ -19,29 +22,51 @@ const HeroSection = () => {
          {/* Hero text and image */}
          <div className="grid auto-rows-auto bg-my-light-blue pt-[88px] 600:grid-cols-2 800:min-h-[450px] 1000:min-h-[500px]">
             {/* Hero text */}
-            <div className="gap-16x4x32 z-10 order-2 flex flex-col justify-center px-[4vw] py-[8vw] 600:order-1 600:pl-[4vw] 600:pr-0 1450:pl-0">
+            <motion.div
+               initial={{ x: -100, opacity: 0 }}
+               animate={{ x: 0, opacity: 1 }}
+               transition={{ duration: 0.3, delay: 0.4 }}
+               className="z-10 order-2 flex flex-col justify-center gap-16x4x32 px-[4vw] py-[8vw] 600:order-1 600:pl-[4vw] 600:pr-0 1450:pl-0"
+            >
                <h1 className="title-text-mark text-h1 leading-none text-my-dark-blue">
                   Capturing Moments That Last a Lifetime
                </h1>
-               <p className="text-h1-para font-light leading-tight text-my-dark-blue">
+               <motion.p
+                  initial={{ y: -50, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.2, delay: 0.5 }}
+                  className="text-h1-para font-light leading-tight text-my-dark-blue"
+               >
                   Welcome to Jill&apos;s Artistic Photography Where Your
                   Treasured Moments Become Timeless Masterpieces You&apos;ll
                   Cherish Forever.
-               </p>
-               <CtaButton />
-            </div>
+               </motion.p>
+               <motion.div
+                  initial={{ y: -50, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.2, delay: 0.6 }}
+               >
+                  <CtaButton />
+               </motion.div>
+            </motion.div>
 
             {/* Hero image */}
             <div className="relative z-[2] order-1 600:order-2">
-               <div className="h-[60vw] min-h-[250px] w-full overflow-clip 600:absolute 600:-top-[4vw] 600:aspect-square 600:h-[70vw] 600:min-h-[520px] 600:w-[70vw] 600:min-w-[520px] 600:rounded-full 1000:max-h-[700px] 1000:max-w-[700px] 1450:max-h-[850px] 1450:max-w-[850px]">
-                  <Image
-                     src={'/jpg/profile/profile_01.jpg'}
-                     alt="Hero Image"
-                     fill
-                     style={{ objectFit: 'cover' }}
-                     objectPosition="30% 10%"
-                  />
-               </div>
+               <motion.div
+                  initial={{ x: 100, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ duration: 0.3, delay: 0.4 }}
+               >
+                  <div className="h-[60vw] min-h-[250px] w-full overflow-clip 600:absolute 600:-top-[4vw] 600:aspect-square 600:h-[70vw] 600:min-h-[520px] 600:w-[70vw] 600:min-w-[520px] 600:rounded-full 1000:max-h-[700px] 1000:max-w-[700px] min-[1390px]:max-h-[850px] min-[1390px]:max-w-[850px]">
+                     <Image
+                        src={'/jpg/profile/profile_01.jpg'}
+                        alt="Hero Image"
+                        fill
+                        style={{ objectFit: 'cover' }}
+                        objectPosition="30% 10%"
+                     />
+                  </div>
+               </motion.div>
             </div>
          </div>
       </Section>
