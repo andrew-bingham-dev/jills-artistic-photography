7de5e2b4d69af95383c09ed4c9950991c2d7cc20
Andrew <andrew.bingham.dev@gmail.com>
2023-12-25 16:27:37 +0700

    Add animation to ServicesSection component

--- app/components/services-section/ServicesSection.tsx
+++ app/components/services-section/ServicesSection.tsx
@@ -1,20 +1,42 @@
+'use client'
+
 import Spacer from '../Spacer'
 import BrowHeadingParagraph from '../brow-heading-paragraph/BrowHeadingParagraph'
 import Section from '../section/Section'
 import ServicesCardContainer from './ServicesCardContainer'
 import PhotoSpread from './PhotoSpread'
+import { motion, useInView } from 'framer-motion'
+import { useRef } from 'react'
 
 const ServicesSection = () => {
+   const ref = useRef(null)
+   const isInView = useInView(ref, {
+      margin: '0px 0px -100px 0px',
+      amount: 'all',
+      once: true,
+   })
+
+   const variants = {
+      visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+      hidden: { opacity: 0, y: 50, transition: { duration: 0.5 } },
+   }
+
    return (
       <Section name="ServicesSection" bgColor="my-medium-blue">
          <div className="pt-[8vw]">
             <div className="px-[4vw] 1450:px-0">
-               <BrowHeadingParagraph
-                  color="my-light-beige"
-                  brow="Capture"
-                  heading="Preserve Your Precious Moments"
-                  paragraph="Professional photography services for weddings, portraits, and more."
-               />
+               <motion.div
+                  ref={ref}
+                  variants={variants}
+                  animate={isInView ? 'visible' : 'hidden'}
+               >
+                  <BrowHeadingParagraph
+                     color="my-light-beige"
+                     brow="Capture"
+                     heading="Preserve Your Precious Moments"
+                     paragraph="Professional photography services for weddings, portraits, and more."
+                  />
+               </motion.div>
                <Spacer size="large" />
                <ServicesCardContainer />
             </div>
