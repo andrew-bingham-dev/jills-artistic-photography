aa02b9e59fcf696f9accd04e0114fec7bd761c65
Andrew <andrew.bingham.dev@gmail.com>
2023-12-25 16:27:41 +0700

    Add animation to ServicesCardContainer

--- app/components/services-section/ServicesCardContainer.tsx
+++ app/components/services-section/ServicesCardContainer.tsx
@@ -2,11 +2,21 @@ import ServicesCard from './ServicesCard'
 import weddingImage04 from '../../../public/jpg/wedding/wedding_04.jpg'
 import portraitImage01 from '../../../public/jpg/portrait/portrait_01.jpg'
 import natureImage04 from '../../../public/jpg/nature/nature_04.jpg'
+import { motion } from 'framer-motion'
 
 const ServicesCardContainer = () => {
    return (
-      <div className="gap-16x4x32 grid auto-rows-fr 600:auto-rows-auto 600:grid-cols-2 900:auto-rows-max 900:grid-cols-3">
-         <div className="600:col-span-2 900:col-span-1">
+      <div className="grid auto-rows-fr gap-16x4x32 600:auto-rows-auto 600:grid-cols-2 900:auto-rows-max 900:grid-cols-3">
+         <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{
+               opacity: 1,
+               y: 0,
+               transition: { duration: 0.5, delay: 0.2 },
+            }}
+            viewport={{ once: true }}
+            className="600:col-span-2 900:col-span-1"
+         >
             <ServicesCard
                brow="Moments"
                heading="Wedding Photography"
@@ -15,21 +25,41 @@ const ServicesCardContainer = () => {
                altImageDescription="Wedding photo"
                imagePosition="center 65%"
             />
-         </div>
-         <ServicesCard
-            brow="Expressions"
-            heading="Portrait Photography"
-            paragraph="Preserve your unique personality in stunning portraits."
-            image={portraitImage01}
-            altImageDescription="Portrait photo"
-         />
-         <ServicesCard
-            brow="Life"
-            heading="Nature Photography"
-            paragraph="Discover the beauty of the natural world through captivating photographs."
-            image={natureImage04}
-            altImageDescription="Nature photo"
-         />
+         </motion.div>
+         <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{
+               opacity: 1,
+               y: 0,
+               transition: { duration: 0.5, delay: 0.4 },
+            }}
+            viewport={{ once: true }}
+         >
+            <ServicesCard
+               brow="Expressions"
+               heading="Portrait Photography"
+               paragraph="Preserve your unique personality in stunning portraits."
+               image={portraitImage01}
+               altImageDescription="Portrait photo"
+            />
+         </motion.div>
+         <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{
+               opacity: 1,
+               y: 0,
+               transition: { duration: 0.5, delay: 0.6 },
+            }}
+            viewport={{ once: true }}
+         >
+            <ServicesCard
+               brow="Life"
+               heading="Nature Photography"
+               paragraph="Discover the beauty of the natural world through captivating photographs."
+               image={natureImage04}
+               altImageDescription="Nature photo"
+            />
+         </motion.div>
       </div>
    )
 }
