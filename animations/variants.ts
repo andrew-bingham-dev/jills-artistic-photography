217f305ab5f4a5f2b0abcde23b67a1daad1533e4
Andrew <andrew.bingham.dev@gmail.com>
2023-12-26 17:46:34 +0700

    Add riseAndAppear animation variant

---
+++ animations/variants.ts
@@ -0,0 +1,13 @@
+export const riseAndAppear = {
+   hidden: {
+      opacity: 0,
+      y: 50
+   },
+   show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+         duration: 0.4
+      }
+   }
+}
