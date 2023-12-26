2f33ec46e88b3620121f8d7fd07295802cb03cf7
Andrew <andrew.bingham.dev@gmail.com>
2023-12-26 21:11:19 +0700

    Add animation to testimonial section

--- app/components/testimonial-section/Testimonial.tsx
+++ app/components/testimonial-section/Testimonial.tsx
@@ -1,5 +1,6 @@
 import starSvg from '../../../public/svg/Star.svg'
 import Image, { StaticImageData } from 'next/image'
+import { motion } from 'framer-motion'
 
 interface Testimonial {
    quote: string
@@ -7,16 +8,42 @@ interface Testimonial {
    name: string
 }
 
+const containerVariants = {
+   hidden: { opacity: 0 },
+   show: {
+      opacity: 1,
+      transition: {
+         staggerChildren: 0.1,
+         delayChildren: 1,
+      },
+   },
+}
+
 const Testimonial: React.FC<Testimonial> = ({ quote, avatar, name }) => {
    return (
-      <div className="gap-16x4x24 flex flex-col text-my-light-beige">
-         <div className="flex flex-row gap-1">
-            <Image src={starSvg} alt="Star" />
-            <Image src={starSvg} alt="Star" />
-            <Image src={starSvg} alt="Star" />
-            <Image src={starSvg} alt="Star" />
-            <Image src={starSvg} alt="Star" />
-         </div>
+      <div className="flex flex-col gap-16x4x24 text-my-light-beige">
+         <motion.div
+            variants={containerVariants}
+            initial="hidden"
+            whileInView="show"
+            className="flex flex-row gap-1"
+         >
+            <motion.div variants={containerVariants}>
+               <Image src={starSvg} alt="Star" />
+            </motion.div>
+            <motion.div variants={containerVariants}>
+               <Image src={starSvg} alt="Star" />
+            </motion.div>
+            <motion.div variants={containerVariants}>
+               <Image src={starSvg} alt="Star" />
+            </motion.div>
+            <motion.div variants={containerVariants}>
+               <Image src={starSvg} alt="Star" />
+            </motion.div>
+            <motion.div variants={containerVariants}>
+               <Image src={starSvg} alt="Star" />
+            </motion.div>
+         </motion.div>
          <p className="text-para italic">&quot;{quote}&quot;</p>
          <div className="flex items-center gap-4">
             <Image src={avatar} alt="Avatar" />
