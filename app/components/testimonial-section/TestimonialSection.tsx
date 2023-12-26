5eb249d922d0938a2d4c9a6e246abcba40083aa4
Andrew <andrew.bingham.dev@gmail.com>
2023-12-26 17:50:47 +0700

    Refactor TestimonialSection component to include RiseAndAppear motion

--- app/components/testimonial-section/TestimonialSection.tsx
+++ app/components/testimonial-section/TestimonialSection.tsx
@@ -1,34 +1,41 @@
-import Spacer from '../Spacer'
+'use client'
+
 import BrowHeadingParagraph from '../brow-heading-paragraph/BrowHeadingParagraph'
 import Section from '../section/Section'
 import Testimonial from './Testimonial'
 import avatar1 from '../../../public/png/avatars/john_doe.png'
 import avatar2 from '../../../public/png/avatars/jane_smith.png'
+import RiseAndAppear from '../motions/rise-and-appear'
 
-interface TestimonialSectionProps {}
-
-const TestimonialSection: React.FC<TestimonialSectionProps> = ({}) => {
+const TestimonialSection: React.FC = () => {
    return (
       <Section name="TestimonialSection" bgColor="my-medium-blue">
-         <div className="gap-y-40x6x80 flex flex-col px-[4vw] py-[8vw] 1450:px-0">
+         <div className="flex flex-col gap-y-40x6x80 px-[4vw] py-[8vw] 1450:px-0">
             <div className="h-12 600:hidden" />
-            <BrowHeadingParagraph
-               heading="My Happy Clients"
-               paragraph="Read about my client's experiences"
-               color="my-light-beige"
-            />
+
+            <RiseAndAppear>
+               <BrowHeadingParagraph
+                  heading="My Happy Clients"
+                  paragraph="Read about my client's experiences"
+                  color="my-light-beige"
+               />
+            </RiseAndAppear>
 
             <div className="grid auto-rows-auto gap-16 800:grid-cols-2">
-               <Testimonial
-                  name="John Doe"
-                  avatar={avatar1}
-                  quote="I couldn't be happier with the stunning wedding photos Jill captured. Her attention to detail and artistic eye truly made our special day unforgettable."
-               />
-               <Testimonial
-                  name="Jane Smith"
-                  avatar={avatar2}
-                  quote="Working with Jill was a pleasure. Her professionalism and talent shine through in every portrait she takes. I highly recommend her services."
-               />
+               <RiseAndAppear>
+                  <Testimonial
+                     name="John Doe"
+                     avatar={avatar1}
+                     quote="I couldn't be happier with the stunning wedding photos Jill captured. Her attention to detail and artistic eye truly made our special day unforgettable."
+                  />
+               </RiseAndAppear>
+               <RiseAndAppear>
+                  <Testimonial
+                     name="Jane Smith"
+                     avatar={avatar2}
+                     quote="Working with Jill was a pleasure. Her professionalism and talent shine through in every portrait she takes. I highly recommend her services."
+                  />
+               </RiseAndAppear>
             </div>
          </div>
       </Section>
