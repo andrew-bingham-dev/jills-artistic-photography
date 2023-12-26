14007e6e0948d1eb8e732e204bc1f62d7cbf84bc
Andrew <andrew.bingham.dev@gmail.com>
2023-12-26 17:50:36 +0700

    Refactor ServicesSection component to use RiseAndAppear motion

--- app/components/services-section/ServicesSection.tsx
+++ app/components/services-section/ServicesSection.tsx
@@ -5,38 +5,21 @@ import BrowHeadingParagraph from '../brow-heading-paragraph/BrowHeadingParagraph
 import Section from '../section/Section'
 import ServicesCardContainer from './ServicesCardContainer'
 import PhotoSpread from './PhotoSpread'
-import { motion, useInView } from 'framer-motion'
-import { useRef } from 'react'
+import RiseAndAppear from '../motions/rise-and-appear'
 
 const ServicesSection = () => {
-   const ref = useRef(null)
-   const isInView = useInView(ref, {
-      margin: '0px 0px -100px 0px',
-      amount: 'all',
-      once: true,
-   })
-
-   const variants = {
-      visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
-      hidden: { opacity: 0, y: 50, transition: { duration: 0.5 } },
-   }
-
    return (
       <Section name="ServicesSection" bgColor="my-medium-blue">
          <div className="pt-[8vw]">
             <div className="px-[4vw] 1450:px-0">
-               <motion.div
-                  ref={ref}
-                  variants={variants}
-                  animate={isInView ? 'visible' : 'hidden'}
-               >
+               <RiseAndAppear>
                   <BrowHeadingParagraph
                      color="my-light-beige"
                      brow="Capture"
                      heading="Preserve Your Precious Moments"
                      paragraph="Professional photography services for weddings, portraits, and more."
                   />
-               </motion.div>
+               </RiseAndAppear>
                <Spacer size="large" />
                <ServicesCardContainer />
             </div>
