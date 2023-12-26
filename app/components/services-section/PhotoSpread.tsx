95ea7947fbced3efb1084a7a27eea7413d60a06f
Andrew <andrew.bingham.dev@gmail.com>
2023-12-26 17:09:14 +0700

    Refactor code to improve performance and readability

--- app/components/services-section/PhotoSpread.tsx
+++ app/components/services-section/PhotoSpread.tsx
@@ -1,91 +1,213 @@
+'use client'
+
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 import RadialGradient from '../radial-gradient/RadialGradient'
+
+const staggerContainerVariants = {
+   initial: {
+      opacity: 0,
+   },
+   animate: {
+      opacity: 1,
+      transition: {
+         staggerChildren: 0.2,
+         when: 'beforeChildren',
+      },
+   },
+}
+
+const staggerItemVariants = {
+   initial: {
+      opacity: 0,
+      scale: 0.8,
+   },
+   animate: {
+      opacity: 1,
+      scale: 1,
+      transition: { duration: 0.5, ease: 'easeInOut' },
+      viewport: {
+         once: true,
+      },
+   },
+}
 
 const PhotoSpread = () => {
    return (
       // TODO: Fix the gradient circle so that it's centered on the image
-      <div className="w-photo-spread relative mt-[12vw] aspect-[5/3.5]">
+      <div className="relative mt-[12vw] aspect-[5/3.5] w-photo-spread">
          {/* gradient circle */}
 
          <RadialGradient left="1" top="40" />
          <RadialGradient left="90" top="170" />
          {/* w:416px h:234px - w:29% */}
-         <div className="hover:z-hover-image absolute left-[4%] top-[2%] z-image aspect-[3/2] w-[29%] -rotate-12 overflow-clip shadow-md transition-transform hover:-rotate-6">
-            <Image
-               src={'/jpg/portrait/portrait_01.jpg'}
-               alt="photo example 1"
-               objectFit="cover"
-               layout="fill"
-               objectPosition="center 20%"
-            />
-         </div>
+         <motion.div
+            variants={staggerContainerVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+         >
+            <motion.div variants={staggerItemVariants}>
+               <motion.div
+                  initial={{ rotate: '0deg' }}
+                  animate={{ rotate: '12deg' }}
+                  whileHover={{
+                     scale: 1.1,
+                     rotate: '0deg',
+                     zIndex: 6,
+                  }}
+                  className="absolute left-[4%] top-[2%] z-image aspect-[3/2] w-[29%] overflow-clip shadow-md"
+               >
+                  <Image
+                     src={'/jpg/portrait/portrait_01.jpg'}
+                     alt="photo example 1"
+                     objectFit="cover"
+                     layout="fill"
+                     objectPosition="center 20%"
+                  />
+               </motion.div>
+            </motion.div>
 
-         {/* w:416px h:415px - w:29% */}
-         <div className="hover:z-hover-image absolute left-[24%] top-[5%] z-image aspect-square w-[29%] -rotate-2 shadow-md transition-transform hover:rotate-0">
-            <Image
-               src={'/jpg/portrait/portrait_05.jpg'}
-               alt="photo example 2"
-               objectFit="cover"
-               layout="fill"
-               objectPosition="center 80%"
-            />
-         </div>
+            {/* w:416px h:415px - w:29% */}
+            <motion.div layout variants={staggerItemVariants}>
+               <motion.div
+                  layout
+                  initial={{ rotate: '0deg' }}
+                  animate={{ rotate: '-2deg' }}
+                  whileHover={{
+                     scale: 1.1,
+                     rotate: '0deg',
+                     zIndex: 6,
+                  }}
+                  className="absolute left-[24%] top-[5%] z-image aspect-square w-[29%] shadow-md"
+               >
+                  <Image
+                     src={'/jpg/portrait/portrait_05.jpg'}
+                     alt="photo example 2"
+                     objectFit="cover"
+                     layout="fill"
+                     objectPosition="center 80%"
+                  />
+               </motion.div>
+            </motion.div>
 
-         {/* w:416px h:234px - w:29% */}
-         <div className="hover:z-hover-image absolute right-[6%] top-[9%] z-image aspect-[2/1] w-[29%] rotate-6 shadow-md transition-transform hover:rotate-3">
-            <Image
-               src={'/jpg/portrait/portrait_02.jpg'}
-               alt="photo example 3"
-               objectFit="cover"
-               layout="fill"
-               objectPosition="center 55%"
-            />
-         </div>
+            {/* w:416px h:234px - w:29% */}
+            <motion.div layout variants={staggerItemVariants}>
+               <motion.div
+                  layout
+                  initial={{ rotate: '0deg' }}
+                  animate={{ rotate: '6deg' }}
+                  whileHover={{
+                     scale: 1.1,
+                     rotate: '0deg',
+                     zIndex: 6,
+                  }}
+                  className="absolute right-[6%] top-[9%] z-image aspect-[2/1] w-[29%] shadow-md"
+               >
+                  <Image
+                     src={'/jpg/portrait/portrait_02.jpg'}
+                     alt="photo example 3"
+                     objectFit="cover"
+                     layout="fill"
+                     objectPosition="center 55%"
+                  />
+               </motion.div>
+            </motion.div>
 
-         {/* w:416px h:416px - w:29% */}
-         <div className="hover:z-hover-image absolute left-[5%] top-[24%] z-image aspect-square w-[29%] rotate-12 shadow-md transition-transform hover:rotate-6">
-            <Image
-               src={'/jpg/nature/nature_01.jpg'}
-               alt="photo example 4"
-               objectFit="cover"
-               layout="fill"
-               objectPosition="center 20%"
-            />
-         </div>
+            {/* w:416px h:416px - w:29% */}
+            <motion.div layout variants={staggerItemVariants}>
+               <motion.div
+                  layout
+                  initial={{ rotate: '0deg' }}
+                  animate={{ rotate: '12deg' }}
+                  whileHover={{
+                     scale: 1.1,
+                     rotate: '0deg',
+                     zIndex: 6,
+                  }}
+                  className="absolute left-[5%] top-[24%] z-image aspect-square w-[29%] shadow-md"
+               >
+                  <Image
+                     src={'/jpg/nature/nature_01.jpg'}
+                     alt="photo example 4"
+                     objectFit="cover"
+                     layout="fill"
+                     objectPosition="center 20%"
+                  />
+               </motion.div>
+            </motion.div>
 
-         {/* w:416px h:416px - w:29% */}
-         <div className="hover:z-hover-image absolute left-[43%] top-[23%] z-image aspect-square w-[29%] rotate-[25deg] shadow-md transition-transform hover:rotate-[20deg]">
-            <Image
-               src={'/jpg/nature/nature_05.jpg'}
-               alt="photo example 5"
-               objectFit="cover"
-               layout="fill"
-               objectPosition="center 20%"
-            />
-         </div>
+            {/* w:416px h:416px - w:29% */}
+            <motion.div layout variants={staggerItemVariants}>
+               <motion.div
+                  layout
+                  initial={{ rotate: '0deg' }}
+                  animate={{ rotate: '25deg' }}
+                  whileHover={{
+                     scale: 1.1,
+                     rotate: '0deg',
+                     zIndex: 6,
+                  }}
+                  className="absolute left-[43%] top-[23%] z-image aspect-square w-[29%] shadow-md"
+               >
+                  <Image
+                     src={'/jpg/nature/nature_05.jpg'}
+                     alt="photo example 5"
+                     objectFit="cover"
+                     layout="fill"
+                     objectPosition="center 20%"
+                  />
+               </motion.div>
+            </motion.div>
 
-         {/* w:515px h:515px - w40% */}
-         <div className="hover:z-hover-image absolute left-[15%] top-[42%] z-image aspect-square w-[35%] -rotate-[6deg] shadow-md transition-transform hover:rotate-0">
-            <Image
-               src={'/jpg/wedding/wedding_02.jpg'}
-               alt="photo example 6"
-               objectFit="cover"
-               layout="fill"
-               objectPosition="center 20%"
-            />
-         </div>
+            {/* w:515px h:515px - w40% */}
+            <motion.div layout variants={staggerItemVariants}>
+               <motion.div
+                  layout
+                  initial={{ rotate: '0deg' }}
+                  animate={{ rotate: '-6deg' }}
+                  whileHover={{
+                     scale: 1.1,
+                     rotate: '0deg',
+                     zIndex: 6,
+                  }}
+                  className="absolute left-[15%] top-[42%] z-image aspect-square w-[35%] shadow-md"
+               >
+                  <Image
+                     src={'/jpg/wedding/wedding_02.jpg'}
+                     alt="photo example 6"
+                     objectFit="cover"
+                     layout="fill"
+                     objectPosition="center 20%"
+                  />
+               </motion.div>
+            </motion.div>
 
-         {/* w:712px h:712px - w:29% */}
-         <div className="hover:z-hover-image absolute left-[50%] top-[45%] z-image aspect-square w-[49%] rotate-[9deg] shadow-md transition-transform hover:rotate-3">
-            <Image
-               src={'/jpg/wedding/wedding_01.jpg'}
-               alt="photo example 7"
-               objectFit="cover"
-               layout="fill"
-               objectPosition="center 20%"
-            />
-         </div>
+            {/* w:712px h:712px - w:29% */}
+
+            <motion.div layout variants={staggerItemVariants}>
+               <motion.div
+                  layout
+                  initial={{ rotate: '0deg' }}
+                  animate={{ rotate: '9deg' }}
+                  whileHover={{
+                     scale: 1.1,
+                     rotate: '0deg',
+                     zIndex: 6,
+                  }}
+                  className="absolute left-[50%] top-[45%] z-image aspect-square w-[49%] shadow-md"
+               >
+                  <Image
+                     src={'/jpg/wedding/wedding_01.jpg'}
+                     alt="photo example 7"
+                     objectFit="cover"
+                     layout="fill"
+                     objectPosition="center 20%"
+                  />
+               </motion.div>
+            </motion.div>
+         </motion.div>
       </div>
    )
 }
