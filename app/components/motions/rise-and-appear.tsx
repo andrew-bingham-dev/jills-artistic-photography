dd3e71d965fb0a3eca7ec8718a6629ef7e71ea0e
Andrew <andrew.bingham.dev@gmail.com>
2023-12-26 17:46:40 +0700

    Add RiseAndAppear motion component

---
+++ app/components/motions/rise-and-appear.tsx
@@ -0,0 +1,21 @@
+import { riseAndAppear } from '@/animations/variants'
+import { motion } from 'framer-motion'
+
+interface RiseAndAppearProps {
+   children: React.ReactNode
+}
+
+const RiseAndAppear: React.FC<RiseAndAppearProps> = ({ children }) => {
+   return (
+      <motion.div
+         variants={riseAndAppear}
+         initial="hidden"
+         whileInView="show"
+         viewport={{ margin: '0px 0px -20% 0px', once: true }}
+      >
+         {children}
+      </motion.div>
+   )
+}
+
+export default RiseAndAppear
