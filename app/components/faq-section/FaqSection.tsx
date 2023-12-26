571c824d5561860a75fdfc6a335de5e4ef86bfdb
Andrew <andrew.bingham.dev@gmail.com>
2023-12-26 17:52:27 +0700

    Refactor FaqSection component to use RiseAndAppear motion

--- app/components/faq-section/FaqSection.tsx
+++ app/components/faq-section/FaqSection.tsx
@@ -1,24 +1,32 @@
-import Spacer from '../Spacer'
+'use client'
+
 import BrowHeadingParagraph from '../brow-heading-paragraph/BrowHeadingParagraph'
+import RiseAndAppear from '../motions/rise-and-appear'
 import Section from '../section/Section'
 import FaqAccordion from './FaqAccordion'
 
 const FaqSection: React.FC = () => {
    return (
       <Section name="FAQSection" bgColor="my-light-blue">
-         <div className="gap-y-40x6x80 flex flex-col px-[4vw] py-[8vw] 1450:px-0">
-            <BrowHeadingParagraph
-               color="my-dark-blue"
-               heading="FAQs"
-               paragraph="Find answers to common questions about my services and process."
-            />
-            <FaqAccordion />
-            <BrowHeadingParagraph
-               headingType="h4"
-               color="my-dark-blue"
-               heading="Still Have Questions?"
-               paragraph="Contact me directly to discuss your photography needs."
-            />
+         <div className="flex flex-col gap-y-40x6x80 px-[4vw] py-[8vw] 1450:px-0">
+            <RiseAndAppear>
+               <BrowHeadingParagraph
+                  color="my-dark-blue"
+                  heading="FAQs"
+                  paragraph="Find answers to common questions about my services and process."
+               />
+            </RiseAndAppear>
+            <RiseAndAppear>
+               <FaqAccordion />
+            </RiseAndAppear>
+            <RiseAndAppear>
+               <BrowHeadingParagraph
+                  headingType="h4"
+                  color="my-dark-blue"
+                  heading="Still Have Questions?"
+                  paragraph="Contact me directly to discuss your photography needs."
+               />
+            </RiseAndAppear>
          </div>
       </Section>
    )
