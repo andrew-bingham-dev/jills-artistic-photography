273365d39ed22921dbec69cffc1a2bd4201d2da7
Andrew <andrew.bingham.dev@gmail.com>
2023-12-26 19:56:23 +0700

    Refactor CtaSection component to use RiseAndAppear motion

--- app/components/cta-section/CtaSection.tsx
+++ app/components/cta-section/CtaSection.tsx
@@ -1,21 +1,27 @@
-import Spacer from '../Spacer'
+'use client'
+
 import Section from '../section/Section'
 import CtaButton from '../cta-button/CtaButton'
+import RiseAndAppear from '../motions/rise-and-appear'
 
 const CtaSection: React.FC = () => {
    return (
       <Section name="CtaSection" bgColor="my-medium-blue">
-         <div className="gap-y-40x6x80 grid auto-rows-auto px-[4vw] py-[8vw] text-my-light-beige 800:grid-cols-2 800:grid-rows-1 1450:px-0">
-            <h2 className="text-h2">Capture Your Special Moments</h2>
-            <div className="gap-16x4x24 flex flex-col">
-               <p className="text-para">
-                  I&apos;m Jill Thompson, your go-to freelance photographer for
-                  capturing life&apos;s most beautiful moments. From weddings
-                  and portraits to nature scenes, I&apos;m committed to creating
-                  images you&apos;ll treasure for a lifetime.
-               </p>
-               <CtaButton />
-            </div>
+         <div className="grid auto-rows-auto gap-y-40x6x80 px-[4vw] py-[8vw] text-my-light-beige 800:grid-cols-2 800:grid-rows-1 1450:px-0">
+            <RiseAndAppear>
+               <h2 className="text-h2">Capture Your Special Moments</h2>
+            </RiseAndAppear>
+            <RiseAndAppear>
+               <div className="flex flex-col gap-16x4x24">
+                  <p className="text-para">
+                     I&apos;m Jill Thompson, your go-to freelance photographer
+                     for capturing life&apos;s most beautiful moments. From
+                     weddings and portraits to nature scenes, I&apos;m committed
+                     to creating images you&apos;ll treasure for a lifetime.
+                  </p>
+                  <CtaButton />
+               </div>
+            </RiseAndAppear>
          </div>
       </Section>
    )
