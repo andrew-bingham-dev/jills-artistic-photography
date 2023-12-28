7180eb6368f4655e9e2ac37a1e80e431d405da06
Andrew <andrew.bingham.dev@gmail.com>
2023-12-28 14:19:28 +0700

    Refactor Spacer component

--- app/components/Spacer.tsx
+++ app/components/Spacer.tsx
@@ -1,48 +1,50 @@
-import React from "react";
+import React from 'react'
 
 interface SpacerProps {
-	direction?: "horizontal" | "vertical";
-	size?: "small" | "medium" | "large" | "x-large";
+   direction?: 'horizontal' | 'vertical'
+   size?: 'small' | 'medium' | 'large' | 'x-large' | 'nav'
 }
 
 const Spacer: React.FC<SpacerProps> = ({
-	direction = "vertical",
-	size = "medium",
+   direction = 'vertical',
+   size = 'medium',
 }) => {
-	const horizontalPadding = () => {
-		switch (size) {
-			case "small":
-				return "px-2";
-			case "medium":
-				return "px-4";
-			case "large":
-				return "px-8";
-			case "x-large":
-				return "px-16";
-			default:
-				return "";
-		}
-	};
+   const horizontalPadding = () => {
+      switch (size) {
+         case 'small':
+            return 'px-2'
+         case 'medium':
+            return 'px-4'
+         case 'large':
+            return 'px-8'
+         case 'x-large':
+            return 'px-16'
+         default:
+            return ''
+      }
+   }
 
-	const verticalPadding = () => {
-		switch (size) {
-			case "small":
-				return "py-2";
-			case "medium":
-				return "py-4";
-			case "large":
-				return "py-8";
-			case "x-large":
-				return "py-16";
-			default:
-				return "";
-		}
-	};
+   const verticalPadding = () => {
+      switch (size) {
+         case 'small':
+            return 'py-2'
+         case 'medium':
+            return 'py-4'
+         case 'large':
+            return 'py-8'
+         case 'x-large':
+            return 'py-16'
+         case 'nav':
+            return 'pt-[88px]'
+         default:
+            return ''
+      }
+   }
 
-	const spacerClassName =
-		direction === "horizontal" ? horizontalPadding() : verticalPadding();
+   const spacerClassName =
+      direction === 'horizontal' ? horizontalPadding() : verticalPadding()
 
-	return <div className={spacerClassName} />;
-};
+   return <div className={spacerClassName} />
+}
 
-export default Spacer;
+export default Spacer
