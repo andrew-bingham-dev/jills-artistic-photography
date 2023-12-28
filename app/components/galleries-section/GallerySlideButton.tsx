9448ec8742780412c11e35555d8f217618cfd2f4
Andrew <andrew.bingham.dev@gmail.com>
2023-12-28 14:21:17 +0700

    Add GallerySlideButton component

---
+++ app/components/galleries-section/GallerySlideButton.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+interface GallerySlideButtonProps {
+   direction: 'left' | 'right'
+   onClick: () => void
+}
+
+const GallerySlideButton: React.FC<GallerySlideButtonProps> = ({
+   direction,
+   onClick,
+}) => {
+   return (
+      <button
+         className="border-1 aspect-square rounded-full border border-my-dark-blue p-3"
+         onClick={() => onClick()}
+      >
+         {direction === 'left' && (
+            <svg
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24"
+               strokeWidth={1.5}
+               stroke="currentColor"
+               className="h-6 w-6"
+            >
+               <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+               />
+            </svg>
+         )}
+         {direction === 'right' && (
+            <svg
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24"
+               strokeWidth={1.5}
+               stroke="currentColor"
+               className="h-6 w-6"
+            >
+               <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+               />
+            </svg>
+         )}
+      </button>
+   )
+}
+
+export default GallerySlideButton
