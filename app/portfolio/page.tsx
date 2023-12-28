41681d304b8a6ee4349ec88fd681eec2c3526c41
Andrew <andrew.bingham.dev@gmail.com>
2023-12-28 14:21:52 +0700

    Add page component with navbar and galleries section

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,13 @@
+import Footer from '../components/footer-section/Footer'
+import GalleriesSection from '../components/galleries-section/GalleriesSection'
+import Navbar from '../components/navbar/Navbar'
+
+export default function Home() {
+   return (
+      <main>
+         <Navbar />
+         <GalleriesSection />
+         {/* <Footer /> */}
+      </main>
+   )
+}
