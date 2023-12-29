ada98824e01cee425073783be8c8b796cbb93ae3
Andrew <andrew.bingham.dev@gmail.com>
2023-12-29 16:09:51 +0700

    Add RiseAndAppear component

--- app/components/motions/RiseAndAppear.tsx
+++ app/components/motions/RiseAndAppear.tsx
@@ -3,13 +3,18 @@ import { motion } from 'framer-motion'
 
 interface RiseAndAppearProps {
    children: React.ReactNode
+   animDelay?: number
 }
 
-const RiseAndAppear: React.FC<RiseAndAppearProps> = ({ children }) => {
+const RiseAndAppear: React.FC<RiseAndAppearProps> = ({
+   children,
+   animDelay,
+}) => {
    return (
       <motion.div
          variants={riseAndAppear}
          initial="hidden"
+         transition={{ delay: animDelay ? animDelay : 0 }}
          whileInView="show"
          viewport={{ amount: 'some', margin: '0px 0px -20% 0px', once: true }}
       >
