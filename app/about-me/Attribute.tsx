60391879e2a5f2810750bd6e363fe695ebfd6233
Andrew <andrew.bingham.dev@gmail.com>
2023-12-29 10:58:04 +0700

    Add Attribute and AttributesSection components

---
+++ app/about-me/Attribute.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+interface AttributeProps {
+   iconUrl: string
+   heading: string
+   description: string[]
+}
+
+const Attribute: React.FC<AttributeProps> = ({
+   iconUrl,
+   heading,
+   description,
+}) => {
+   return <div></div>
+}
+
+export default Attribute
