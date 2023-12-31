007338b92fe1c6f4a064336237f6f4de4e9ed7bf
Andrew <andrew.bingham.dev@gmail.com>
2023-12-31 16:20:43 +0700

    Refactor CtaButton class names

--- app/components/cta-button/CtaButton.tsx
+++ app/components/cta-button/CtaButton.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 
 const CtaButton = () => {
    return (
-      <Button className="select-none btn text-button max-w-fit bg-my-medium-orange text-my-dark-blue shadow-xl hover:bg-my-dark-blue hover:text-my-medium-orange">
+      <Button className="btn relative max-w-fit select-none bg-my-medium-orange text-button text-my-dark-blue shadow-xl hover:bg-my-dark-blue hover:text-my-medium-orange">
          Contact Me
       </Button>
    )
